1b56038fa7e893854f1e4251ee8ef6c663118c82
Leonid Pogozhev <lpogojev@gmail.com>
2017-11-21 21:50:59 +0200

    Add solveWithSteams method to ProblemSolver class

--- src/main/java/hillelee/reflection/ProblemSolver.java
+++ src/main/java/hillelee/reflection/ProblemSolver.java
@@ -4,12 +4,16 @@ import java.lang.reflect.InvocationTargetException;
 import java.lang.reflect.Method;
 import java.util.Arrays;
 import java.util.Optional;
+import java.util.function.Function;
 import java.util.stream.Collectors;
+import java.util.stream.Stream;
 
 /**
  * Created by JavaEE on 28.10.2017.
  */
 public class ProblemSolver {
+
+    //Classic Java
     public String solve(Object problem){
         Class<?> aClass = problem.getClass();
         Method[] methods = aClass.getMethods();
@@ -26,11 +30,23 @@ public class ProblemSolver {
         throw new RuntimeException("There is no CorrectAnswer annotation");
     }
 
-//    public Optional<String> solveWithStreams(Object problem) {
-//        Class<?> aClass = problem.getClass();
-//        Method[] methods = aClass.getMethods();
-//        Optional<String> method = Arrays.stream(methods)
-//                .filter(m -> m.isAnnotationPresent(CorrectAnswer.class))
-//                .map(m -> m.toString());
-//    }
+    //Using Stream API
+    public String solveWithStreams(Object problem) {
+        return Stream.of(problem)
+                .map(Object::getClass)
+                .flatMap(clazz -> Arrays.stream(clazz.getMethods()))
+                .filter(m -> m.isAnnotationPresent(CorrectAnswer.class))
+                .map(invokeOn(problem))
+                .findFirst()
+                .orElseThrow(() -> new RuntimeException("There is no CorrectAnswer annotation"));
+    }
+       private Function<Method, String> invokeOn(Object problem) {
+           return method -> {
+               try {
+                   return (String) method.invoke(problem);
+               } catch (Exception e) {
+                   throw new RuntimeException(e);
+               }
+           };
+       }
 }
